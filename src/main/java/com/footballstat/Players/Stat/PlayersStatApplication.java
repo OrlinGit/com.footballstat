25d0ddb0fea5647e953d8002c26bf4e48e378f40
OrlinGit <orlin.nikolov@hotmail.com>
2026-02-28 15:44:31 +0200

    "Basic structure of the app created"

---
+++ src/main/java/com/footballstat/Players/Stat/PlayersStatApplication.java
@@ -0,0 +1,13 @@
+package com.footballstat.Players.Stat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersStatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersStatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footballstat/Players/Stat/PlayersStatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballstat.Players.Stat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersStatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
