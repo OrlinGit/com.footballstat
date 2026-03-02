3c111e804c1125ca3ef300c175ee853052baed35
OrlinGit <orlin.nikolov@hotmail.com>
2026-03-02 22:29:54 +0200

    In DBInitializer logic was implemented not to duplicate the data in the Database.

---
+++ src/main/java/com/footballstat/Players/Stat/DTO/TeamDTO.java
@@ -0,0 +1,7 @@
+package com.footballstat.Players.Stat.DTO;
+
+public class TeamDTO {
+
+
+
+}
--- src/main/java/com/footballstat/Players/Stat/utility/DBInitializer.java
+++ src/main/java/com/footballstat/Players/Stat/utility/DBInitializer.java
@@ -1,5 +1,9 @@
 package com.footballstat.Players.Stat.utility;
 
+import com.footballstat.Players.Stat.repository.MatchesRepo;
+import com.footballstat.Players.Stat.repository.PlayerRepo;
+import com.footballstat.Players.Stat.repository.RecordsRepo;
+import com.footballstat.Players.Stat.repository.TeamRepo;
 import com.footballstat.Players.Stat.services.MatchesImportService;
 import com.footballstat.Players.Stat.services.PlayerImportService;
 import com.footballstat.Players.Stat.services.RecordsImportServices;
@@ -17,22 +21,43 @@ public class DBInitializer {
     private final MatchesImportService matchesImportService;
     private final RecordsImportServices recordsImportServices;
 
+    private final TeamRepo teamRepo;
+    private final PlayerRepo playerRepo;
+    private final MatchesRepo matchesRepo;
+    private final RecordsRepo recordsRepo;
+
     public DBInitializer(TeamImportService teamImportService,
                          PlayerImportService playerImportService,
                          MatchesImportService matchesImportService,
-                         RecordsImportServices recordsImportServices) {
+                         RecordsImportServices recordsImportServices,
+                         TeamRepo teamRepo,
+                         PlayerRepo playerRepo,
+                         MatchesRepo matchesRepo,
+                         RecordsRepo recordsRepo) {
         this.teamImportService = teamImportService;
         this.playerImportService = playerImportService;
         this.matchesImportService = matchesImportService;
         this.recordsImportServices = recordsImportServices;
+        this.teamRepo = teamRepo;
+        this.playerRepo = playerRepo;
+        this.matchesRepo = matchesRepo;
+        this.recordsRepo = recordsRepo;
     }
 
     @PostConstruct
 
     public void initializeDB() throws IOException {
-        teamImportService.importTeams();
-        playerImportService.importPlayers();
-        matchesImportService.importMatches();
-        recordsImportServices.importRecords();
+        if ( teamRepo.count() == 0){
+            teamImportService.importTeams();
+        }
+        if (playerRepo.count() == 0) {
+            playerImportService.importPlayers();
+        }
+        if (matchesRepo.count() == 0) {
+            matchesImportService.importMatches();
+        }
+        if(recordsRepo.count() == 0) {
+            recordsImportServices.importRecords();
+        }
     }
 }
